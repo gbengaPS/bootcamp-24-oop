076f0119d422922fc2b376ce9746bc2fe9c71fb8
gbenga_ps <ioyetade@gmail.com>
2017-07-04 15:28:35 +0100

    adds 11 tests cases for the oop.js program

--- oop.js
+++ oop.js
@@ -3,23 +3,25 @@ class Mamalia{
 
   constructor(habitat,domesticated,gestation){
 
-        let homeostasis = 'warm-blooded';
-        let fertilization = 'Internal';
-        let limbs = 4;
+        let _homeostasis = 'warm-blooded';
+        let _fertilization = 'Internal';
+        let _limbs = 4;
         this.habitat = habitat;
         this.domesticated = domesticated;
         this.gestation = gestation;
 
 
-        this.getLimbs = function(){
-          return limbs;
+        this._getLimbs = function(){
+          return _limbs;
+        }
+        this._setLimbs=function(limb){
+          return _limbs=limb;
+        }
+        this._getFertilization = function(){
+          return _fertilization + " fertilization.";
         }
 
-        this.getFertilization = function(){
-          return fertilization + " fertilization.";
-        }
-
-        this.getHomeostasis = function(){
+        this._getHomeostasis = function(){
           return homeostasis;
         }
 
@@ -27,7 +29,7 @@ class Mamalia{
 
   makeSound(sound){
     if(typeof(sound) !='string'){
-      return undefined
+      return false;
     }
     else{
       let result = 'Animal '+sound;
@@ -57,32 +59,46 @@ class Primate extends Mamalia{
 
     constructor(habitat,domesticated,gestation){
       //Encapsulation
-        let numberOfToes = 5;
-        let hasNails=9;
+        const _numberOfToes = 5;
+        let _hasNails=9;
         super(habitat,domesticated,gestation);
-         this.hasNails ='Animals has nails instead of claws';
-        this.getHasNails = function(){
+         this._hasNails ='Animals has nails instead of claws';
+        this._getHasNails = function(){
           return hasNails;
         }
+
+    }
+
+
+    buildFamily(primate1,primate2){
+      if(typeof primate1 =='string' && typeof primate2=='string'){
+        let married= primate1+" marries "+primate2;
+        return married;
+      }
+      
+      else{
+        return false;
+      }
+
     }
 
 }
-
+'use strict';
 class Canivora extends Mamalia{
   
   constructor(habitat,domesticated,gestation){
-        let numberOfToes = 4;
-        let hunts;
+        const _numberOfToes = 4;
+        let _hunts;
         if(domesticated==false){
-          hunts = 'Animal hunts';
+          _hunts = 'Animal hunts';
         }
         else{
-          hunts = 'Animal does not hunts';
+          _hunts = 'Animal does not hunts';
         }
         super(habitat,domesticated,gestation);
 
-        this.getHunts = function(){
-          return hunts;
+        this._getHunts = function(){
+          return _hunts;
         }
 
         
@@ -99,15 +115,17 @@ class Rodentia extends Mamalia{
 
 module.exports.mammals = Mamalia;
 module.exports.rodents = Rodentia;
+module.exports.primates = Primate;
 //Testing the classes
 
 let dog = new Canivora('terestrial',true,8)
 let monkey= new Primate('terestrial',true,9)
-
+let x = new Mamalia('aborea',true,3);
 console.log(dog.gestation);
-console.log(dog.getFertilization());
+console.log(dog._getFertilization());
 console.log(dog.reproduce(1));
 console.log(dog.makeSound('barks'));
 
-console.log(monkey.gestation);
-console.log(monkey.hasNails);
+console.log(monkey._gestation);
+console.log(monkey._hasNails);
+console.log(x._getFertilization())
--- test/test.js
+++ test/test.js
@@ -1,12 +1,26 @@
 const assert  = require('chai').assert;
 const Mammal = require('../oop.js').mammals;
 const Rodent = require('../oop.js').rodents;
+const Primate = require('../oop.js').primates;
+
+//Mammal tests
 describe('Mammal',function(){
+   let dog= new Mammal('terestrial',true,6);
   it('should contain a constructor method',function(){
       assert.deepEqual(typeof Mammal.constructor(),'function');
   });
-  it('')
+  it('should contain private variables',function(){
+    assert.equal(typeof Mammal.prototype.limbs,'undefined');
+  })
+  it('should contain a method that gets a private variable',function(){
+    assert.isOk( dog._getLimbs(),'message')
+  })
+  it('should contain a method that verifies its inputs',function(){
+      assert.notOk(Mammal.prototype.makeSound(343434),'should return false');
+  });
 });
+
+//Rodent tests
 describe('Rodent',function(){
   it('should contain a constructor method',function(){
     assert.deepEqual(typeof Rodent.constructor(),'function');
@@ -15,3 +29,24 @@ describe('Rodent',function(){
     assert.equal(Rodent.prototype instanceof Mammal,true);
   })
 })
+
+
+//Primate tests
+describe('Primate',function(){
+  let monkey= new Primate('terestrial',true,9);
+  it('should contain the  constructor function' ,function(){
+      assert.equal(typeof Primate.constructor,'function')
+  });
+  it('should inherit the Mammal class',function(){
+      assert.equal(Primate.prototype instanceof Mammal,true);
+  });
+  it('should contain a method that verifies its inputs',function(){
+      assert.notOk(Primate.prototype.buildFamily(2,4),'');
+  });
+  it('should always return an integer for the number of limbs',function(){
+    assert.equal(typeof eval(monkey._getLimbs()),'number');
+  });
+  it('number of limbs should not be modifiable',function(){
+      assert.notOk(monkey._setLimbs(),'should return error')
+  });
+});
